cef4ff5996e8895d0d653c0e9e4e56e745fb1333
HenrikSpa <henrik.spangmyr@midvatten.se>
2023-03-31 08:57:59 +0000

    Fixes bug when "Show selected on map" isn't working (fixes #13) (patch by TurboGraphxBeige). Fixes bug where reported calculation progress exceeded 100%. Fixes sorting bug for fields with mixed strings and floats (fixes #12). Adjusted sort to put None, '' and NaN-values last.

--- tests/test_groupstats.py
+++ tests/test_groupstats.py
@@ -25,6 +25,11 @@ class TestGroupStats(object):
     def setUp(self):
         pass
 
+    def tearDown(self):
+        QgsProject.instance().clear()
+        self.gs.dlg.close()
+        self.gs = None
+
     def create_vectorlayer(self, _fields, data, geometries=None, geomtype='Point'):
         self.vlayer = QgsVectorLayer("{}?crs=epsg:4326".format(geomtype), "test", "memory")
         provider = self.vlayer.dataProvider()
@@ -1305,7 +1310,141 @@ class TestGroupStats(object):
                  call.write('row2;7.0\r\n'),
                  call.close()] in mock_file.mock_calls
 
-    def tearDown(self):
-        QgsProject.instance().clear()
-        self.gs.dlg.close()
-        self.gs = None
+    def test_sortcol_floats(self):
+        _fields = [QgsField('id', QVariant.Int, QVariant.typeToName(QVariant.Int)),
+                   QgsField('rows', QVariant.String, QVariant.typeToName(QVariant.String)),
+                   QgsField('cols', QVariant.String, QVariant.typeToName(QVariant.String)),
+                   QgsField('values', QVariant.Double, QVariant.typeToName(QVariant.Double))]
+        #data = [[1, 'row1', 'col1', 1.0], [2, 1.0, 'col1', 2.0], [3, 'row2', 'col1', 3.0], [4, 2.0, 'col1', 4.0],
+        #        [5, '1', 'col2', 5.0], [6, 1, 'col2', 6.0], [7, '2', 'col2', 7.0], [8, 'row2', 'col2', 8.0]]
+        data = [[1, 'row1', 'col1', 1.0],
+                [2, '1.0', 'col1', 2.0],
+                [3, 'row2', 'col1', 3.0],
+                [4, '2.0', 'col1', 4.0],
+                [5, '1', 'col2', 5.0],
+                [6, '1', 'col2', 6.0],
+                [7, '2', 'col2', 7.0],
+                [8, 'row2', 'col2', 8.0],
+                [9, '1', 'col1', 20.0],
+                [10, '1', 'col1', 10.0]]
+        self.create_vectorlayer(_fields, data)
+        self.init_gs()
+
+        #for idx, value in enumerate(getattr(self.gs.dlg.tm1, self.data_var)):
+        #    print("{} {}".format(str(idx), str(value)))
+
+        cols_index = self.all.createIndex(0, 0)
+        rows_index = self.all.createIndex(2, 0)
+        values_index = self.all.createIndex(3, 0)
+        calc_index = self.all.createIndex(10, 0)
+
+        cols_data = self.all.mimeData([cols_index])
+        rows_data = self.all.mimeData([rows_index])
+        values_data = self.all.mimeData([values_index])
+        calc_data = self.all.mimeData([calc_index])
+
+        self.columns_model.dropMimeData(cols_data, None, 0, 0, QModelIndex())
+        self.rows_model.dropMimeData(rows_data, None, 0, 0, QModelIndex())
+        self.values_model.dropMimeData(calc_data, None, 0, 0, self.values_model.index(0))
+        self.values_model.dropMimeData(values_data, None, 0, 0, self.values_model.index(0))
+
+        @mock.patch("PyQt5.QtWidgets.QFileDialog.selectedFiles")
+        @mock.patch('PyQt5.QtWidgets.QMainWindow.statusBar')
+        @mock.patch("PyQt5.QtWidgets.QMessageBox.information")
+        @mock.patch("PyQt5.QtWidgets.QFileDialog.exec_")
+        @mock.patch("builtins.open")
+        def savefile(dlg, mock_open, mock_exec, mock_messagebox, mock_statusbar, mock_selectedfile):
+            mock_selectedfile.return_value = ['noname']
+            mock_exec.return_value = 1
+            getattr(self.gs.dlg, self.showScore)()
+            qheaderview = self.gs.dlg.ui.result.horizontalHeader()
+            qheaderview.setSortIndicator(2, Qt.AscendingOrder)
+            #self.gs.dlg.sortRows(self, row, mode)
+
+            mock_file = mock.MagicMock(spec=TextIOBase)
+            mock_open.return_value = mock_file
+            getattr(dlg.ui, self.actionSaveCSV).trigger()
+            return mock_file
+
+        mock_file = savefile(self.gs.dlg)
+        print(str(mock_file.mock_calls))
+
+        assert [call.write('cols;col1;col2\r\n'),
+                 call.write('rows;;\r\n'),
+                 call.write('2;;7.0\r\n'),
+                 call.write('row2;3.0;8.0\r\n'),
+                 call.write('1;30.0;11.0\r\n'),
+                 call.write('1.0;2.0;\r\n'),
+                 call.write('2.0;4.0;\r\n'),
+                 call.write('row1;1.0;\r\n'),
+                 call.close()] in mock_file.mock_calls
+
+
+    def test_sortcol_contains_None(self):
+        _fields = [QgsField('id', QVariant.Int, QVariant.typeToName(QVariant.Int)),
+                   QgsField('rows', QVariant.String, QVariant.typeToName(QVariant.String)),
+                   QgsField('cols', QVariant.String, QVariant.typeToName(QVariant.String)),
+                   QgsField('values', QVariant.Double, QVariant.typeToName(QVariant.Double))]
+        #data = [[1, 'row1', 'col1', 1.0], [2, 1.0, 'col1', 2.0], [3, 'row2', 'col1', 3.0], [4, 2.0, 'col1', 4.0],
+        #        [5, '1', 'col2', 5.0], [6, 1, 'col2', 6.0], [7, '2', 'col2', 7.0], [8, 'row2', 'col2', 8.0]]
+        data = [[1, 'row1', 'col1', 1.0],
+                [2, '1.0', 'col1', 2.0],
+                [3, 'row2', 'col1', 3.0],
+                [4, '2.0', 'col1', 4.0],
+                [5, '1', 'col2', 5.0],
+                [6, '1', 'col2', 6.0],
+                [7, '2', 'col2', 7.0],
+                [8, 'row2', 'col2', None],
+                [9, '1', 'col1', 20.0],
+                [10, '1', 'col1', 10.0]]
+        self.create_vectorlayer(_fields, data)
+        self.init_gs()
+
+        #for idx, value in enumerate(getattr(self.gs.dlg.tm1, self.data_var)):
+        #    print("{} {}".format(str(idx), str(value)))
+
+        cols_index = self.all.createIndex(0, 0)
+        rows_index = self.all.createIndex(2, 0)
+        values_index = self.all.createIndex(3, 0)
+        calc_index = self.all.createIndex(10, 0)
+
+        cols_data = self.all.mimeData([cols_index])
+        rows_data = self.all.mimeData([rows_index])
+        values_data = self.all.mimeData([values_index])
+        calc_data = self.all.mimeData([calc_index])
+
+        self.columns_model.dropMimeData(cols_data, None, 0, 0, QModelIndex())
+        self.rows_model.dropMimeData(rows_data, None, 0, 0, QModelIndex())
+        self.values_model.dropMimeData(calc_data, None, 0, 0, self.values_model.index(0))
+        self.values_model.dropMimeData(values_data, None, 0, 0, self.values_model.index(0))
+
+        @mock.patch("PyQt5.QtWidgets.QFileDialog.selectedFiles")
+        @mock.patch('PyQt5.QtWidgets.QMainWindow.statusBar')
+        @mock.patch("PyQt5.QtWidgets.QMessageBox.information")
+        @mock.patch("PyQt5.QtWidgets.QFileDialog.exec_")
+        @mock.patch("builtins.open")
+        def savefile(dlg, mock_open, mock_exec, mock_messagebox, mock_statusbar, mock_selectedfile):
+            mock_selectedfile.return_value = ['noname']
+            mock_exec.return_value = 1
+            getattr(self.gs.dlg, self.showScore)()
+            qheaderview = self.gs.dlg.ui.result.horizontalHeader()
+            qheaderview.setSortIndicator(2, Qt.AscendingOrder)
+            #self.gs.dlg.sortRows(self, row, mode)
+
+            mock_file = mock.MagicMock(spec=TextIOBase)
+            mock_open.return_value = mock_file
+            getattr(dlg.ui, self.actionSaveCSV).trigger()
+            return mock_file
+
+        mock_file = savefile(self.gs.dlg)
+        print(str(mock_file.mock_calls))
+
+        assert [call.write('cols;col1;col2\r\n'),
+                 call.write('rows;;\r\n'),
+                 call.write('2;;7.0\r\n'),
+                 call.write('1;30.0;11.0\r\n'),
+                 call.write('1.0;2.0;\r\n'),
+                 call.write('2.0;4.0;\r\n'),
+                 call.write('row1;1.0;\r\n'),
+                 call.write('row2;3.0;\r\n'),
+                 call.close()] in mock_file.mock_calls
